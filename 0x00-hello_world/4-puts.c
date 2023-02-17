3d21369a0ce4d5e76512ef2d968c408e233b82c9
edidiongapostle <eididongapostle@gmail.com>
2023-02-16 16:58:15 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * A program that prints a line of code using puts
+ * Return: 0
+ */
+init main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
