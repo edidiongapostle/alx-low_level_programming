ce02b3f5e96894202e45478ba4624b67c5e91739
edidiongapostle <eididongapostle@gmail.com>
2023-02-17 19:16:24 -0800

    Smile in the mirror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,14 +3,14 @@
 /**
  * main - A program that prints the lowercase alphabet in reverse.
  *
- * Return: A;ways 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 	char alph;
 
-	for (alph = 'z'; letter >= 'a'; alph--)
+	for (alph = 'z'; alph >= 'a'; alph--)
 		putchar(alph);
 
 	putchar('\n');
