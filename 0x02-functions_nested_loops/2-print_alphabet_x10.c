3c875f805e3b4bc1e4a9369d723d9a8891bef7e8
edidiongapostle <eididongapostle@gmail.com>
2023-02-21 13:13:04 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - A function that prints 10 times the alphabet
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char alph;
+
+	while (count++ <= 9)
+	{
+		for (alph = 'a'; alph <= 'z'; alph++)
+			_putchar(alph);
+		_putchar('\n');
+	}
+}
