143920ae086f40175c2c3c40a6c1e83149604a76
edidiongapostle <eididongapostle@gmail.com>
2023-02-21 14:12:32 -0800

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,8 @@
 
 /**
  * _islower - A functiom that checks for lowercase character
- * Return: Always 0
+ *@c: parameter to checked
+ * Return: 1 if it is an alphabet and 0 otherwise
  */
 
 int _islower(int c)
