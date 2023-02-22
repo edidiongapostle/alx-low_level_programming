718c80c52c83e80503e2001c0ef94175376f98d0
edidiongapostle <eididongapostle@gmail.com>
2023-02-21 22:30:02 -0800

    Battery Street

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,6 +5,7 @@
  * print_to_98 - A program to print all natural numbers from n to 98
  *
  * @n : the number to begin counting from
+ * Return: 0
  */
 
 void print_to_98(int n)
@@ -12,13 +13,13 @@ void print_to_98(int n)
 	if (n >= 98)
 	{
 		while (n > 98)
-			printf("%d, " n--);
+			printf("%d, ", n--);
 		printf("%d\n", n);
 	}
 	else
 	{
 		while (n < 98)
-			printf("%d, " n++);
+			printf("%d, ", n++);
 		printf("%d\n", n);
 	}
 }
