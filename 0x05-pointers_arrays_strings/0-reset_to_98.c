ed7a00a2bd91fb83255f297c1fe7e71cc16c5550
edidiongapostle <eididongapostle@gmail.com>
2023-02-27 14:18:42 -0800

    98 Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - a funtioln that takes a pointer to an int
+ * as a parameter and updated the value
+ * @n: the int to be checked
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
