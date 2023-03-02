19bd4ce9c2e6f50382ff2d5c521480bc757723c2
edidiongapostle <eididongapostle@gmail.com>
2023-03-01 18:48:57 -0800

    I am a kind of paranoid in reverse

---
+++ 0x06-pointers_arrays_strings/1-main.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+{
+	int a, b;
+
+	for (a = 0; dest[a] != '\0'; a++)
+	{
+		continue;
+	}
+	for (b = 0; src[b] != '\0' && b < n; b++)
+	{
+		dest[a + b] = src[b];
+	}
+	dest[a + b] = '\0';
+	return (dest);
+}
