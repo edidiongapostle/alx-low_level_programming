27812313aac77ed1c74846488743b6e00795a077
edidiongapostle <eididongapostle@gmail.com>
2023-03-07 07:28:24 -0800

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
-/** _memset - a function that fills memory with a constant byte
+/**
+ * _memset - a function that fills memory with a constant byte
  * @s: the constant in byte
  *
  * @n: bte of the memory area pointed by s
