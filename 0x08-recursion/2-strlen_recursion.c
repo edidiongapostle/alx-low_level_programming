d00c99a344bba0e94a4874370043b4f2c9620519
edidiongapostle <eididongapostle@gmail.com>
2023-03-08 17:08:53 -0800

    strlen_recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,12 +10,9 @@
 
 int _strlen_recursion(char *s)
 {
-	int n = 1 + _strlen_recursion(s + 1);
-
 	if (*s == '\0')
 	{
-		return(0);
+		return (0);
 	}
-
-	return(n);
+	return (1 + _strlen_recursion(s + 1));
 }
