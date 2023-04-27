17cd74a406568028983570425a24896b27919a26
edidiongapostle <edidiongapostle@gmail.com>
2023-04-27 00:07:14 -0700

    a program that copies the content of a file to another file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,15 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
+
+#define BUFFER_SIZE 1024
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
 #include <stdio.h>
 
 int _putchar(char c);
